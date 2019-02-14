98900167b116d338545b2c11b6a9f247f3ddb5ce
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-02-14 14:27:46 +0100

    Implement CustomStack structure Implement CustomStackTest

--- src/main/java/com/codecool/CustomStack.java
+++ src/main/java/com/codecool/CustomStack.java
@@ -1,4 +1,43 @@
 package com.codecool;
 
-public class CustomStack {
+public class CustomStack<T> {
+
+    private static class Node<T> {
+        
+    }
+
+    private int size;
+    private T data;
+
+    public CustomStack (int initialSize) {
+        this.size = initialSize;
+    }
+
+    public void push(T data) throws StackOverflow {
+
+    }
+
+    public T pop() throws StackUnderflow {
+        return (T) data;
+    }
+
+    public T peek() {
+        return (T) data;
+    }
+
+    public int getSpace() {
+        return 0;
+    }
+
+    public int size(){
+        return this.size;
+    }
+}
+
+class StackOverflow extends Exception {
+
+}
+
+class StackUnderflow extends Exception {
+
 }
--- src/test/java/com/codecool/CustomStackTest.java
+++ src/test/java/com/codecool/CustomStackTest.java
@@ -1,7 +1,73 @@
 package com.codecool;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class CustomStackTest {
+
+    @Test
+    public void testCustomStackCreation() {
+        CustomStack<String> customStack = new CustomStack<>(12);
+    }
+
+    @Test
+    public void checkIfUserCanPushAString() throws StackOverflow {
+        CustomStack<String> customStack = new CustomStack<>(5);
+        customStack.push("string");
+        assertEquals(1, customStack.size());
+    }
+
+    @Test
+    public void checkIfUserCanPushIntegers() throws StackOverflow {
+        CustomStack<Integer> customStack = new CustomStack<>(5);
+        customStack.push(99);
+        assertEquals(1, customStack.size());
+    }
+
+    @Test
+    public void getCustomStackSize() throws StackOverflow {
+        CustomStack<String> customStack = new CustomStack<>(5);
+        customStack.push("string");
+        assertEquals(1, customStack.size());
+    }
+
+    @Test
+    public void checkIfUserCanPopAString() throws StackOverflow, StackUnderflow {
+        CustomStack<String> customStack = new CustomStack<>(5);
+        customStack.push("string");
+        assertEquals("string", customStack.pop());
+        assertEquals(0, customStack.size());
+    }
+
+    @Test
+    public void checkIfUserCanPeekAString() throws StackOverflow {
+        CustomStack<String> customStack = new CustomStack<>(5);
+        customStack.push("string");
+        assertEquals("string", customStack.peek());
+        assertEquals(1, customStack.size());
+    }
+
+    @Test
+    public void getFreeSpacesLeft() throws StackOverflow {
+        CustomStack<String> customStack = new CustomStack<>(5);
+        customStack.push("string");
+        assertEquals(4, customStack.getSpace());
+    }
+
+    @Test
+    public void throwsStackOverflownExceptionWhenAddingToFullStack() throws StackOverflow {
+        CustomStack<String> customStack = new CustomStack<>(3);
+        customStack.push("string");
+        customStack.push("string");
+        customStack.push("string");
+        assertThrows(StackOverflow.class, ()-> customStack.push("string"));
+    }
+
+    @Test
+    public void throwsStackUnderflowExceptionWhenPoppingEmptyStack() throws StackUnderflow{
+        CustomStack<String> customStack = new CustomStack<>(3);
+        assertThrows(StackUnderflow.class, ()-> customStack.pop());
+    }
 
 }
